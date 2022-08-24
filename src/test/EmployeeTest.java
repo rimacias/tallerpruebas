29b9aba1ff3a64db4e32a1fe8284b0b8c0dfd84a
Ramón Macías <rimacias@espol.edu.ec>
2022-08-24 10:26:49 -0500

    Initial commit

---
+++ src/test/EmployeeTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class EmployeeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testEmployee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateYearBonus() {
+		fail("Not yet implemented");
+	}
+
+}
