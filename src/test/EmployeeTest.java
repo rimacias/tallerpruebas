41f7286a0e1093fad7ebca6121733ce41bc0d288
Ramón Macías <rimacias@espol.edu.ec>
2022-08-27 00:15:28 -0500

    Finalización

--- src/test/EmployeeTest.java
+++ src/test/EmployeeTest.java
@@ -12,13 +12,34 @@ import org.junit.Test;
 import tallerpruebas.*;
 
 public class EmployeeTest {
-	int mes = LocalDate.now().getMonthValue();
-	Employee dollarWorker = new Employee(1000f, "USD", 10f, EmployeeType.Worker);
-	Employee euroWorker = new Employee(1000f, "euro", 10f, EmployeeType.Worker);
-	Employee usdManager = new Employee(3000f, "USD", 25f, EmployeeType.Manager);
-	Employee euManager = new Employee(3000f, "EU", 25f, EmployeeType.Manager);
+	static int mes = LocalDate.now().getMonthValue();;
+	
+	static Employee dollarWorker;
+	static Employee euroWorker;
+	static Employee usdManager;
+	static Employee euManager;
+	static BigDecimal CS_USD_W_EXPECTED;
+	static BigDecimal CS_EU_W_EXPECTED;
+	static BigDecimal CS_USD_M_EXPECTED;
+	static BigDecimal CS_EU_M_EXPECTED;
+	static BigDecimal CYB_W_EXPECTED;
+	static BigDecimal CYB_USD_M_EXPECTED;
+	static BigDecimal CYB_EU_M_EXPECTED;
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		Boolean isEven = mes % 2 == 0;
+		dollarWorker = new Employee(1000f, "USD", 10f, EmployeeType.Worker);
+		euroWorker = new Employee(1000f, "euro", 10f, EmployeeType.Worker);
+		usdManager = new Employee(3000f, "USD", 25f, EmployeeType.Manager);
+		euManager = new Employee(3000f, "EU", 25f, EmployeeType.Manager);
+		CS_USD_M_EXPECTED = isEven ? new BigDecimal(3017.5) : new BigDecimal(3081.8333333333);
+		CS_EU_M_EXPECTED = isEven ? new BigDecimal(2867.5) : new BigDecimal(2931.8333333333);
+		CS_USD_W_EXPECTED = isEven ? new BigDecimal(1000) : new BigDecimal(1064 + 1 / 3);
+		CS_EU_W_EXPECTED = isEven ? new BigDecimal(950) : new BigDecimal(1014 + 1 / 3);
+		CYB_W_EXPECTED = new BigDecimal(386);
+		CYB_USD_M_EXPECTED = new BigDecimal(3386);
+		CYB_EU_M_EXPECTED = new BigDecimal(3236);
 	}
 
 	@AfterClass
@@ -42,24 +63,17 @@ public class EmployeeTest {
 
 	@Test
 	public void testCs() {
-		if(mes%2!=0) {
-			assertEquals(new BigDecimal(1064+1/3), new BigDecimal(dollarWorker.cs()));
-			assertEquals(new BigDecimal(1014+1/3), new BigDecimal(euroWorker.cs()));
-			assertEquals(new BigDecimal(usdManager.cs()), new BigDecimal(3081.8333333333));
-			assertEquals(new BigDecimal(euManager.cs()), new BigDecimal(2931.8333333333));
-		}else {
-			assertEquals(new BigDecimal(1000), new BigDecimal(dollarWorker.cs()));
-			assertEquals(new BigDecimal(950), new BigDecimal(euroWorker.cs()));
-			assertEquals(new BigDecimal(usdManager.cs()), new BigDecimal(3017.5));
-			assertEquals(new BigDecimal(euManager.cs()), new BigDecimal(2867.5));
-		}
+		assertEquals(new BigDecimal(dollarWorker.cs()), CS_USD_W_EXPECTED);
+		assertEquals(new BigDecimal(euroWorker.cs()), CS_EU_W_EXPECTED);
+		assertEquals(new BigDecimal(usdManager.cs()), CS_USD_M_EXPECTED);
+		assertEquals(new BigDecimal(euManager.cs()), CS_EU_M_EXPECTED);
 	}
 
 	@Test
 	public void testCalculateYearBonus() {
-		assertEquals(new BigDecimal(386), new BigDecimal(dollarWorker.CalculateYearBonus()));
-		assertEquals(new BigDecimal(usdManager.CalculateYearBonus()), new BigDecimal(3386));
-		assertEquals(new BigDecimal(euManager.CalculateYearBonus()), new BigDecimal(3236));
+		assertEquals(new BigDecimal(dollarWorker.CalculateYearBonus()), CYB_W_EXPECTED);
+		assertEquals(new BigDecimal(usdManager.CalculateYearBonus()), CYB_USD_M_EXPECTED);
+		assertEquals(new BigDecimal(euManager.CalculateYearBonus()), CYB_EU_M_EXPECTED);
 	}
 
 }
