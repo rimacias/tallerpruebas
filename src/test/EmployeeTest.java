b40a179624237b44f1823eeda59ef0f166d0bdf1
Ramón Macías <rimacias@espol.edu.ec>
2022-08-26 22:56:10 -0500

    Finalización

--- src/test/EmployeeTest.java
+++ src/test/EmployeeTest.java
@@ -3,23 +3,20 @@ package test;
 import static org.junit.Assert.*;
 
 import java.math.BigDecimal;
-
+import java.time.LocalDate;
 import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 import tallerpruebas.*;
-<<<<<<< HEAD
 
 public class EmployeeTest {
-=======
->>>>>>> 1c77f4a7fba1c2af833838e8c93d816e6194e5cd
-
-public class EmployeeTest {
-	
+	int mes = LocalDate.now().getMonthValue();
+	Employee dollarWorker = new Employee(1000f, "USD", 10f, EmployeeType.Worker);
+	Employee euroWorker = new Employee(1000f, "euro", 10f, EmployeeType.Worker);
+	Employee usdManager = new Employee(3000f, "USD", 25f, EmployeeType.Manager);
+	Employee euManager = new Employee(3000f, "EU", 25f, EmployeeType.Manager);
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -30,7 +27,7 @@ public class EmployeeTest {
 
 	@Before
 	public void setUp() throws Exception {
-		
+
 	}
 
 	@After
@@ -39,34 +36,30 @@ public class EmployeeTest {
 
 	@Test
 	public void testEmployee() {
-<<<<<<< HEAD
-		assertNotNull(new Employee(260.6f,"USD",0.2f,EmployeeType.Worker));
-=======
+		assertNotNull(new Employee(260.6f, "USD", 0.2f, EmployeeType.Worker));
 		assertNotNull(new Employee(3000f, "USD", 0.15f, EmployeeType.Manager));
->>>>>>> 1c77f4a7fba1c2af833838e8c93d816e6194e5cd
 	}
 
 	@Test
 	public void testCs() {
-<<<<<<< HEAD
-		Employee dollarWorker = new Employee(1000f,"USD",10f,EmployeeType.Worker);
-		Employee euroWorker = new Employee(1000f,"euro",10f,EmployeeType.Worker);
-		Employee dollarSupervisor = new Employee(1000f,"USD",10f,EmployeeType.Supervisor);
-		Employee euroSupervisor = new Employee(1000f,"euro",10f,EmployeeType.Supervisor);
-		assertEquals(new BigDecimal(1000),new BigDecimal(dollarWorker.cs()));
-		
-		
-=======
-		Employee usdManager = new Employee(3000f, "USD", 15f, EmployeeType.Manager);
-		Employee euManager = new Employee(3000f, "EU", 25f, EmployeeType.Manager);
-		Assert.assertEquals(new BigDecimal (usdManager.cs()), new BigDecimal(0));
-		Assert.assertEquals(new BigDecimal (euManager.cs()), new BigDecimal(0));
->>>>>>> 1c77f4a7fba1c2af833838e8c93d816e6194e5cd
+		if(mes%2!=0) {
+			assertEquals(new BigDecimal(1064+1/3), new BigDecimal(dollarWorker.cs()));
+			assertEquals(new BigDecimal(1014+1/3), new BigDecimal(euroWorker.cs()));
+			assertEquals(new BigDecimal(usdManager.cs()), new BigDecimal(3081.8333333333));
+			assertEquals(new BigDecimal(euManager.cs()), new BigDecimal(2931.8333333333));
+		}else {
+			assertEquals(new BigDecimal(1000), new BigDecimal(dollarWorker.cs()));
+			assertEquals(new BigDecimal(950), new BigDecimal(euroWorker.cs()));
+			assertEquals(new BigDecimal(usdManager.cs()), new BigDecimal(3017.5));
+			assertEquals(new BigDecimal(euManager.cs()), new BigDecimal(2867.5));
+		}
 	}
 
 	@Test
 	public void testCalculateYearBonus() {
-		fail("Not yet implemented");
+		assertEquals(new BigDecimal(386), new BigDecimal(dollarWorker.CalculateYearBonus()));
+		assertEquals(new BigDecimal(usdManager.CalculateYearBonus()), new BigDecimal(3386));
+		assertEquals(new BigDecimal(euManager.CalculateYearBonus()), new BigDecimal(3236));
 	}
 
 }
